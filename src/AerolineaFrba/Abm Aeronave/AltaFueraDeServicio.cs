672b4949ded06ec8d2afb6c4bd8f8d0a795eda6d
tsandler <toby.s92@gmail.com>
2015-11-30 02:16:47 -0300

    arranco con la modificacion

---
+++ src/AerolineaFrba/Abm Aeronave/AltaFueraDeServicio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Abm_Aeronave
+{
+    public partial class AltaFueraDeServicio : Form
+    {
+        public AltaFueraDeServicio()
+        {
+            InitializeComponent();
+            this.cargarComboBox();
+        }
+
+        public void cargarComboBox()
+        {
+            string query = "SELECT matricula FROM JUST_DO_IT.Aeronaves WHERE baja_fuera_servicio = 1";
+            SqlDataReader reader = Server.getInstance().query(query);
+            while (reader.Read())
+            {
+                this.comboBox1.Items.Add(reader["matricula"].ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
