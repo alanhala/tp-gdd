eabb935503a55d78aa6eca5879219c20aaeea4d6
tsandler <toby.s92@gmail.com>
2015-11-30 02:29:04 -0300

    subo lo que tengo de la modificacion de rutas, falta aun

--- src/AerolineaFrba/Abm Ruta/Listado Rutas.Designer.cs
+++ src/AerolineaFrba/Abm Ruta/Listado Rutas.Designer.cs
@@ -33,6 +33,7 @@ namespace AerolineaFrba.Abm_Ruta
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Listado_Rutas));
             this.listadoDeRutas = new System.Windows.Forms.DataGridView();
             this.seleccionarRuta = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.listadoDeRutas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,17 +42,17 @@ namespace AerolineaFrba.Abm_Ruta
             this.listadoDeRutas.AllowUserToAddRows = false;
             this.listadoDeRutas.AllowUserToDeleteRows = false;
             this.listadoDeRutas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.listadoDeRutas.Location = new System.Drawing.Point(39, 114);
+            this.listadoDeRutas.Location = new System.Drawing.Point(130, 72);
             this.listadoDeRutas.Name = "listadoDeRutas";
             this.listadoDeRutas.ReadOnly = true;
             this.listadoDeRutas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.listadoDeRutas.Size = new System.Drawing.Size(824, 404);
+            this.listadoDeRutas.Size = new System.Drawing.Size(659, 295);
             this.listadoDeRutas.TabIndex = 0;
             this.listadoDeRutas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.listadoDeRutas_CellContentClick);
             // 
             // seleccionarRuta
             // 
-            this.seleccionarRuta.Location = new System.Drawing.Point(609, 548);
+            this.seleccionarRuta.Location = new System.Drawing.Point(651, 396);
             this.seleccionarRuta.Name = "seleccionarRuta";
             this.seleccionarRuta.Size = new System.Drawing.Size(147, 48);
             this.seleccionarRuta.TabIndex = 1;
@@ -59,12 +60,23 @@ namespace AerolineaFrba.Abm_Ruta
             this.seleccionarRuta.UseVisualStyleBackColor = true;
             this.seleccionarRuta.Click += new System.EventHandler(this.seleccionarRuta_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Listado de Rutas";
+            // 
             // Listado_Rutas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1017, 618);
+            this.ClientSize = new System.Drawing.Size(894, 470);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.seleccionarRuta);
             this.Controls.Add(this.listadoDeRutas);
             this.Name = "Listado_Rutas";
@@ -72,6 +84,7 @@ namespace AerolineaFrba.Abm_Ruta
             this.Load += new System.EventHandler(this.Listado_Rutas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.listadoDeRutas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,5 +92,6 @@ namespace AerolineaFrba.Abm_Ruta
 
         private System.Windows.Forms.DataGridView listadoDeRutas;
         private Button seleccionarRuta;
+        private Label label1;
     }
 }
--- src/AerolineaFrba/Abm Ruta/Modificar Ruta.cs
+++ src/AerolineaFrba/Abm Ruta/Modificar Ruta.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,31 @@ namespace AerolineaFrba.Abm_Ruta
 {
     public partial class Modificar_Ruta : Form
     {
-        public Modificar_Ruta()
+        private double ruta;
+        public Modificar_Ruta() { }
+
+        public Modificar_Ruta(string id)
         {
             InitializeComponent();
+            this.ruta = double.Parse(id);
         }
 
         private void Modificar_Ruta_Load(object sender, EventArgs e)
         {
+            Commons.getInstance().cargarComboBox("Ciudades", "nombre", cmbCiudadOrigen);
+            Commons.getInstance().cargarComboBox("Ciudades", "nombre", cmbCiudadDestino);
+            Commons.getInstance().cargarComboBox("TiposServicios", "nombre", cmbTipoServicio);
 
+            string query = "SELECT * FROM JUST_DO_IT.Rutas WHERE id=" + this.ruta;
+            SqlDataReader reader = Server.getInstance().query(query);
+            reader.Read();
+            txtCodigoRuta.Text = reader["codigo"].ToString();
+            txtPrecioBasePorKg.Text = reader["precio_baseKG"].ToString();
+            txtPrecioBasePorPasaje.Text = reader["precio_basePasaje"].ToString();
+            string id_origen = reader["ciu_id_origen"].ToString();
+            string id_destino = reader["ciu_id_destino"].ToString();
+            string tipo_servicio = reader["tipo_servicio"].ToString();
+            reader.Close();
         }
     }
 }
