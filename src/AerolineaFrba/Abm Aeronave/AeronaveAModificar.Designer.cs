f37ae70bb2ba8385f7f010734ad5cff6b56c2bb4
Elian Rizzo <elian_rizzo93@hotmail.com>
2015-12-01 02:23:42 -0300

    Listado seleccion aeronave

--- src/AerolineaFrba/Abm Aeronave/AeronaveAModificar.Designer.cs
+++ src/AerolineaFrba/Abm Aeronave/AeronaveAModificar.Designer.cs
@@ -29,47 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AeronaveAModificar));
-            this.cbMatricula = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnSeleccionar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox1.SuspendLayout();
+            this.dgvModificarAeronave = new System.Windows.Forms.DataGridView();
+            this.matricula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fabricante = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo_servicio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cant_butacas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.espacio_encomiendas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModificarAeronave)).BeginInit();
             this.SuspendLayout();
             // 
-            // cbMatricula
+            // btnSeleccionar
             // 
-            this.cbMatricula.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbMatricula.FormattingEnabled = true;
-            this.cbMatricula.Location = new System.Drawing.Point(100, 40);
-            this.cbMatricula.Name = "cbMatricula";
-            this.cbMatricula.Size = new System.Drawing.Size(121, 21);
-            this.cbMatricula.TabIndex = 1;
-            this.cbMatricula.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Matricula";
-            // 
-            // btnAceptar
-            // 
-            this.btnAceptar.Location = new System.Drawing.Point(238, 122);
-            this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
-            this.btnAceptar.TabIndex = 3;
-            this.btnAceptar.Text = "Aceptar";
-            this.btnAceptar.UseVisualStyleBackColor = true;
-            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            this.btnSeleccionar.Location = new System.Drawing.Point(578, 338);
+            this.btnSeleccionar.Name = "btnSeleccionar";
+            this.btnSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.btnSeleccionar.TabIndex = 3;
+            this.btnSeleccionar.Text = "Seleccionar";
+            this.btnSeleccionar.UseVisualStyleBackColor = true;
+            this.btnSeleccionar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(148, 122);
+            this.btnCancelar.Location = new System.Drawing.Point(488, 338);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 4;
@@ -77,43 +61,89 @@
             this.btnCancelar.UseVisualStyleBackColor = true;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // groupBox1
+            // dgvModificarAeronave
             // 
-            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.cbMatricula);
-            this.groupBox1.Location = new System.Drawing.Point(28, 16);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(269, 91);
-            this.groupBox1.TabIndex = 53;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Complete el siguiente campo";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.dgvModificarAeronave.AllowUserToAddRows = false;
+            this.dgvModificarAeronave.AllowUserToDeleteRows = false;
+            this.dgvModificarAeronave.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvModificarAeronave.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.matricula,
+            this.modelo,
+            this.fabricante,
+            this.tipo_servicio,
+            this.cant_butacas,
+            this.espacio_encomiendas});
+            this.dgvModificarAeronave.Location = new System.Drawing.Point(12, 46);
+            this.dgvModificarAeronave.MultiSelect = false;
+            this.dgvModificarAeronave.Name = "dgvModificarAeronave";
+            this.dgvModificarAeronave.ReadOnly = true;
+            this.dgvModificarAeronave.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvModificarAeronave.Size = new System.Drawing.Size(641, 286);
+            this.dgvModificarAeronave.TabIndex = 60;
+            // 
+            // matricula
+            // 
+            this.matricula.HeaderText = "Matricula";
+            this.matricula.Name = "matricula";
+            this.matricula.ReadOnly = true;
+            // 
+            // modelo
+            // 
+            this.modelo.HeaderText = "Modelo";
+            this.modelo.Name = "modelo";
+            this.modelo.ReadOnly = true;
+            // 
+            // fabricante
+            // 
+            this.fabricante.HeaderText = "Fabricante";
+            this.fabricante.Name = "fabricante";
+            this.fabricante.ReadOnly = true;
+            // 
+            // tipo_servicio
+            // 
+            this.tipo_servicio.HeaderText = "Tipo de Servicio";
+            this.tipo_servicio.Name = "tipo_servicio";
+            this.tipo_servicio.ReadOnly = true;
+            // 
+            // cant_butacas
+            // 
+            this.cant_butacas.HeaderText = "Cantidad de Butacas";
+            this.cant_butacas.Name = "cant_butacas";
+            this.cant_butacas.ReadOnly = true;
+            // 
+            // espacio_encomiendas
+            // 
+            this.espacio_encomiendas.HeaderText = "Espacio para Encomiendas";
+            this.espacio_encomiendas.Name = "espacio_encomiendas";
+            this.espacio_encomiendas.ReadOnly = true;
             // 
             // AeronaveAModificar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(325, 157);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(668, 373);
+            this.Controls.Add(this.dgvModificarAeronave);
             this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnSeleccionar);
             this.Name = "AeronaveAModificar";
             this.Text = "Seleccionar Aeronave a Modificar";
             this.Load += new System.EventHandler(this.AeronaveAModificar_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvModificarAeronave)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cbMatricula;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnSeleccionar;
         private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvModificarAeronave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn matricula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fabricante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo_servicio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cant_butacas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn espacio_encomiendas;
     }
 }
--- src/AerolineaFrba/Abm Aeronave/AeronaveAModificar.cs
+++ src/AerolineaFrba/Abm Aeronave/AeronaveAModificar.cs
@@ -7,19 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 
 namespace AerolineaFrba.Abm_Aeronave
 {
     public partial class AeronaveAModificar : Form
     {
+        private ObservableCollection<Aeronave> aeronaves;
+
+
         public AeronaveAModificar()
         {
             InitializeComponent();
+            aeronaves = new ObservableCollection<Aeronave>();
+            this.cargarAeronaves();
+            this.dgvModificarAeronave.DataSource = aeronaves;
+            dgvModificarAeronave.Columns["id"].Visible = false;
+        }
+
+        private void cargarAeronaves()
+        {
+            string query =
+                "SELECT aeronaves.id, matricula, modelo, kgs_disponibles, butacas_totales, fabricante , tipoServicio.nombre AS tipo_servicio "
+                    + "FROM JUST_DO_IT.Aeronaves aeronaves, JUST_DO_IT.TiposServicios tipoServicio "
+                    + "WHERE aeronaves.tipo_servicio = tipoServicio.id "
+                    + "ORDER BY aeronaves.id";
+            SqlDataReader reader = Server.getInstance().query(query);
+            while (reader.Read())
+            {
+                var aeronave = new Aeronave();
+                aeronave.id = Convert.ToInt32(reader["id"]);
+                aeronave.matricula = reader["matricula"].ToString();
+                aeronave.modelo = reader["modelo"].ToString();
+                aeronave.kgs_disponibles = Convert.ToInt32(reader["kgs_disponibles"]);
+                aeronave.butacas_totales = Convert.ToInt32(reader["butacas_totales"]);
+                aeronave.fabricante = reader["fabricante"].ToString();
+                aeronave.servicio = reader["tipo_servicio"].ToString();
+
+                aeronaves.Add(aeronave);
+
+            }
+            reader.Close();
         }
 
         private void AeronaveAModificar_Load(object sender, EventArgs e)
         {
-            Commons.getInstance().cargarComboBoxOrderBy("Aeronaves", "matricula", cbMatricula);
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -29,7 +63,9 @@ namespace AerolineaFrba.Abm_Aeronave
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            new modificarAeronave(cbMatricula.Text).Show();
+            Aeronave aeronave = (Aeronave)dgvModificarAeronave.CurrentRow.DataBoundItem;
+            new modificarAeronave(aeronave.matricula).Show();
+            this.Close();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
